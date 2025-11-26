c075f600f9c14bdfe4b00e30b8e06b3f39dc0cc3
kacper-dabrowski <kdtw301@gmail.com>
2025-11-26 15:24:41 +0100

    add subtract feature

--- calculator.spec.ts
+++ calculator.spec.ts
@@ -1,11 +1,19 @@
 import { Calculator } from "./calculator";
 
 describe("calculator", () => {
-  it("should return 1", () => {
+  it("should return add two numbers", () => {
     const calculator = new Calculator();
-    
+
     const result = calculator.add(1, 2);
 
     expect(result).toBe(3);
+  });
+
+  it("should subtract two numbers", () => {
+    const calculator = new Calculator();
+    
+    const result = calculator.subtract(1, 2);
+
+    expect(result).toBe(-1);
   });
 });
--- calculator.ts
+++ calculator.ts
@@ -2,4 +2,8 @@ export class Calculator {
     add(a: number, b: number): number {
         return a + b;
     }
+
+    subtract(a: number, b: number): number {
+        return a - b;
+    }
 }
