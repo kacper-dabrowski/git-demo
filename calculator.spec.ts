df0bcbdc1e5a6a84adbd70ee33d87be66bd85d8c
kacper-dabrowski <kdtw301@gmail.com>
2025-11-26 15:20:23 +0100

    init project

---
+++ calculator.spec.ts
@@ -0,0 +1,10 @@
+import { Calculator } from "./calculator";
+
+describe("calculator", () => {
+  it("should return 1", () => {
+    const calculator = new Calculator();
+    const result = calculator.add(1, 2);
+
+    expect(result).toBe(1);
+  });
+});
